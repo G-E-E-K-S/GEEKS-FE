c52929c39743f1ecde3db46e69c971242d88801c
leesoyuun <leesoyun4206@naver.com>
2023-11-12 22:32:53 +0900

    Add change choose val of dormitory

--- src/components/Join/DormitoryBox.js
+++ src/components/Join/DormitoryBox.js
@@ -1,12 +1,20 @@
 import styled from "styled-components";
+import genderCheck from "../../assets/img/Join/genderCheck.svg";
 
 const DormitoryTotalBox = styled.div`
     height: 9vh;
     border-radius: 12px;
-    background: #EFEFEF;
-    color: #707070;
-    padding-left: 20px;
+    background: ${(props)=>props.state ? '#F7F7F7' : props.isSelected ? '#FFF4CD' :'#EFEFEF'};
+    color: ${(props)=>props.state ? '#B7B7B7' : props.isSelected ? '#865800' : '#707070'};
+    padding:0px 20px;
     margin-bottom: 1.89vh;
+`;
+const DormitoryCheck = styled.div`
+    height: 100%;
+    display: flex;
+    align-items: center;
+    display:flex;
+    justify-content: space-between;
 `;
 const Dormitory = styled.div`
     font-size: 20px;
@@ -15,8 +23,11 @@ const Dormitory = styled.div`
 `;
 const DormitoryBox = (props) => {
     return(
-        <DormitoryTotalBox>
-            <Dormitory>{props.dormitory}</Dormitory>
+        <DormitoryTotalBox onClick={props.onClick} state={props.disable} isSelected={props.isSelected}>
+            <DormitoryCheck>
+                <Dormitory>{props.dormitory}</Dormitory>
+                {props.isSelected ? <img src={genderCheck}/> : null}
+            </DormitoryCheck>
         </DormitoryTotalBox>
     )
 }
