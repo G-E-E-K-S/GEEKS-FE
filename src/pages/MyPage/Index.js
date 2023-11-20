6ce3c192f24ceb0956f71b7c8f5e7171d1d6d77d
leesoyuun <leesoyun4206@naver.com>
2023-11-20 20:24:13 +0900

    Add Setting User Info page

--- src/pages/MyPage/Index.js
+++ src/pages/MyPage/Index.js
@@ -1,6 +1,7 @@
 import MyPage from './MyPage';
 import LifeStyles from './LifeStyles';
 import EditProfile from './EditProfile';
+import SettingUserInfo from './SettingUserInfo';
 
 
-export { MyPage, LifeStyles, EditProfile };
+export { MyPage, LifeStyles, EditProfile, SettingUserInfo };
