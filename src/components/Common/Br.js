3d29cd585071dfe7db9bfe38e932e7e5f71a0290
leesoyuun <leesoyun4206@naver.com>
2024-01-09 23:23:52 +0900

    Solve Br Prob

---
+++ src/components/Common/Br.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const TotalBr = styled.div`
+  display: flex;
+  height: 12px;
+  background: #f7f7f7;
+  width: 100vw;
+  margin-left: calc(-50vw + 50%);
+  margin-top: ${(props)=>props.marginTop};
+  margin-bottom: ${(props)=>props.marginBottom};
+`;
+
+const Br = (props) => {
+  return <TotalBr marginTop={props.marginTop} marginBottom={props.marginBottom}/>;
+};
+export default Br;
