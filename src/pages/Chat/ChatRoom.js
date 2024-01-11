d8211e06a00db9709de01d675bf1cfcca52c7cc5
leesoyuun <leesoyun4206@naver.com>
2024-01-12 00:15:07 +0900

    Add function that we click plus btn on chatting room then chat bottm menu Open

--- src/pages/Chat/ChatRoom.js
+++ src/pages/Chat/ChatRoom.js
@@ -10,6 +10,11 @@ import Dots from "../../assets/img/Home/edit.svg";
 import Plus from "../../assets/img/Chat/add.svg";
 import Send from "../../assets/img/Chat/send.svg";
 import BasicProfile from "../../assets/img/MyPage/basicProfile.svg";
+import Photo from "../../assets/img/Chat/photo.svg";
+import Video from "../../assets/img/Chat/video.svg";
+import Camera from "../../assets/img/Chat/camera.svg";
+import Rule from "../../assets/img/Chat/liveRule.svg";
+import ChatBottomMenu from "../../components/Chat/ChatBottomMenu";
 
 const ChatHeader = styled.div`
   width: 100%;
@@ -48,24 +53,23 @@ const Date = styled.div`
 const ChatBottom = styled.div`
   position: fixed;
   width: 100%;
-  height: 17.06vh;
-  padding: 1.65vh 5.12vw 0 5.12vw;
+  bottom: ${(props)=>props.isBottomOpen ?'28.19vh' : '17.06vh'};
+  padding: 14px 5.12vw 0 5.12vw;
   border-top: 1px solid #efefef;
 `;
 const Add = styled.img`
   width: 32px;
   height: 32px;
-  margin-right: 3.07vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  margin: auto 0;
+  cursor: pointer;
 `;
 const InputMsgBox = styled.div`
   width: 100%;
-  height: 5.21vh;
+  height: 44px;
   padding: 1.18vh 3.07vw 1.18vh 4.1vw;
   border-radius: 24px;
   background: #f7f7f7;
+  margin-left: 3.07vw;
 `;
 const InputMsg = styled.input`
   outline: none;
@@ -82,9 +86,24 @@ const InputMsg = styled.input`
 const SendImg = styled.img`
   float: right;
   cursor: pointer;
+  margin: 0 auto;
+  width: 24px;
+  height: 24px;
+`;
+const PlusMenu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 20px 2.82vw 0 2.82vw;
+  width: 100%;
 `;
 const ChatRoom = () => {
-  const [isSelectMenu, setIsSelectMenu] = useState("findroommate");
+  const [isChatBottomClick, setIsChatBottomClick] = useState(false);
+  
+  const handleChatBottom = () => {
+    setIsChatBottomClick(!isChatBottomClick);
+  }
+
+
   return (
     <c.Totalframe>
       <c.ScreenComponent>
@@ -103,9 +122,9 @@ const ChatRoom = () => {
           <MyChat time={`오후 05:26`}chat={`네 안녕하세요! 아직 구하고 있는 중입니다`}/>
         </c.SubScreen>
       </c.ScreenComponent>
-      <ChatBottom>
+      <ChatBottom isBottomOpen={isChatBottomClick}>
         <c.Flex>
-          <Add src={Plus} />
+          <Add src={Plus} onClick={()=>setIsChatBottomClick(true)}/>
           <InputMsgBox>
             <c.Flex>
               <InputMsg />
@@ -113,6 +132,15 @@ const ChatRoom = () => {
             </c.Flex>
           </InputMsgBox>
         </c.Flex>
+        {isChatBottomClick ? <c.Flex>
+          <PlusMenu>
+            <ChatBottomMenu icon={Photo} iconText={`사진`}/>
+            <ChatBottomMenu icon={Video} iconText={`동영상`}/>
+            <ChatBottomMenu icon={Camera} iconText={`카메라`}/>
+            <ChatBottomMenu icon={Rule} iconText={`생활 규칙`}/>
+          </PlusMenu>
+            
+          </c.Flex> : null}
       </ChatBottom>
     </c.Totalframe>
   );
