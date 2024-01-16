68e9d0c3aaf8fdb97aa081080e9a7c7403301c54
leesoyuun <leesoyun4206@naver.com>
2024-01-16 22:18:27 +0900

    Adding a comment component to the Post page

---
+++ src/components/Community/CommentCnt.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+import CommentIcon from "../../assets/img/Community/comment.svg";
+
+const CommentBox = styled.div`
+  display: flex;
+  padding: 12px 0px;
+`;
+const CommentImg = styled.img`
+  margin: auto 0;
+  width: 16px;
+  height: 16px;
+`;
+const CommentNum = styled.div`
+  color: #949494;
+  font-size: 0.875rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 18px;
+  margin-left: 6px;
+`;
+const CommentCnt = (props) => {
+  return (
+    <CommentBox>
+      <CommentImg src={CommentIcon} />
+      <CommentNum>댓글 {props.number}</CommentNum>
+    </CommentBox>
+  );
+};
+export default CommentCnt;
