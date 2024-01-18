f3e27e5deafc1a2c781cf009af9612de2fae6ed5
leesoyuun <leesoyun4206@naver.com>
2024-01-19 00:01:45 +0900

    Fix Choose Major BottomSheet

---
+++ src/components/Join/Department.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+import * as c from "../../components/Common/CommonStyle";
+import ForwardArrow from "../../assets/img/Join/forwardArrow.svg";
+
+const DepartmentBox = styled.div`
+  height: 64px;
+  padding: 20px 0px;
+  width: 100%;
+`;
+const DepartmentTxt = styled.div`
+  color: #525252;
+  font-size: 1.125rem;
+  font-weight: 600;
+  line-height: 24px;
+`;
+const Department = (props) => {
+  return (
+    <DepartmentBox>
+      <c.SpaceBetween>
+        <DepartmentTxt>{props.department}</DepartmentTxt>
+        <img src={ForwardArrow} />
+      </c.SpaceBetween>
+    </DepartmentBox>
+  );
+};
+export default Department;
