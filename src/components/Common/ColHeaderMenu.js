3531b62660ebcf5999821e28295c7122e0c7eff0
leesoyuun <leesoyun4206@naver.com>
2024-01-28 23:22:34 +0900

    Add colheader menu components

---
+++ src/components/Common/ColHeaderMenu.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+import GoBack from "./GoBack";
+const TotalHeaderMenu = styled.div`
+  width: 100%;
+  margin-top: 52px;
+  padding: 12px 0px;
+  align-items: center;
+`;
+const ColHeaderMenu = (props) => {
+  return (
+    <TotalHeaderMenu>
+      <GoBack />
+      <div>{props.children}</div>
+    </TotalHeaderMenu>
+  );
+};
+export default ColHeaderMenu;
