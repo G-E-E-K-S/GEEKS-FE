256bb47a346e29ffa6b8681b771fb8a06bba1457
leesoyuun <leesoyun4206@naver.com>
2024-01-28 17:40:25 +0900

    Modify comment components to full width

--- src/components/Community/Comment.js
+++ src/components/Community/Comment.js
@@ -7,11 +7,13 @@ import Dots from "../../assets/img/Community/grayDots.svg";
 
 const CommentBox = styled.div`
   height: max-content;
-  width: 100%;
+  width: ${(props)=>props.isRecomment ? `100vw` : `100%`};
   padding-top: 16px;
   padding-bottom: 16px;
   padding-left: ${(props)=>props.paddingLeft};
+  padding-right: ${(props)=>props.paddingRight};
   background-color: ${(props)=>props.isRecomment ? '#F7F7F7' : '#fff'};
+  margin-left: ${(props)=>props.isRecomment && `calc(-50vw + 50%)`};
 `;
 const ThreeDots = styled.img`
   width: 16px;
@@ -34,13 +36,12 @@ const Icons = styled.div`
 `;
 const Comment = (props) => {
   return (
-    <CommentBox paddingLeft={props.paddingLeft} isRecomment={props.recomment}>
+    <CommentBox paddingLeft={props.paddingLeft} isRecomment={props.recomment} paddingRight={props.paddingRight}>
       <c.SpaceBetween>
         <PostInfo
           profileImg={props.postInfo.profileImg}
           username={props.postInfo.username}
-          uploadtime={props.postInfo.uploadtime}
-        />
+          uploadtime={props.postInfo.uploadtime}/>
         <Icons>
           <CommentImg src={CommentIcon} onClick={props.wirteChild}/>
           <ThreeDots src={Dots} />
