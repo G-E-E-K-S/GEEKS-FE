8dea16e0c92e83b3eff7d7e0ecc991f194873ceb
leesoyuun <leesoyun4206@naver.com>
2024-01-29 15:44:18 +0900

    Add bts interaction

--- src/components/Common/Modal.js
+++ src/components/Common/Modal.js
@@ -7,7 +7,7 @@ const ModalOverlay = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background: rgba(0, 0, 0, 0.5);
+  background: rgba(0, 0, 0, 0.2);
   display: flex;
   justify-content: center;
   align-items: center;
