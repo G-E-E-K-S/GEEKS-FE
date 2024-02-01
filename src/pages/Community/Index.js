a1a0b8da56182dd0703f35f599b16ef8aba88532
leesoyuun <leesoyun4206@naver.com>
2024-02-02 01:03:29 +0900

    Add OpenGroup Page

--- src/pages/Community/Index.js
+++ src/pages/Community/Index.js
@@ -4,5 +4,6 @@ import Community from "./Community";
 import MyCommunity from "./MyCommunity";
 import ScrapPost from "./ScrapPost";
 import CommunityWrite from "./CommunityWrite";
+import OpenGroup from "./OpenGroup"
 
-export { WritePost, Post, Community, MyCommunity, ScrapPost, CommunityWrite};
+export { WritePost, Post, Community, MyCommunity, ScrapPost, CommunityWrite, OpenGroup};
