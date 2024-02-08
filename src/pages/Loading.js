ead216c9dbb85172a925eef885c44e0861baaddb
leesoyuun <leesoyun4206@naver.com>
2024-02-08 22:48:22 +0900

    Add: 로띠로 제작한 로딩페이지 추가

---
+++ src/pages/Loading.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+import Lottie from "lottie-react";
+import LoadingImg from "../assets/lottie/loading.json";
+
+const Total = styled.div`
+  background: #000000;
+  opacity: 0.2;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const LottieImg = styled.div`
+  width: 80px;
+  height: 80px;
+`;
+const Loading = () => {
+  return (
+    <Total>
+      <LottieImg>
+        <Lottie animationData={LoadingImg} />
+      </LottieImg>
+    </Total>
+  );
+};
+export default Loading;
