91d74fd9ac57e5642bf03345e4bc47fa37b68d7e
leesoyuun <leesoyun4206@naver.com>
2024-02-12 16:23:11 +0900

    Modify: 배포시 스크롤 버벅임으로 인한 높이 수정

--- src/components/Common/CommonStyle.js
+++ src/components/Common/CommonStyle.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Totalframe = styled.div`
   // width: 390px;
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
   height: -webkit-fill-available;
   height: fill-available;
   margin: 0 auto;
