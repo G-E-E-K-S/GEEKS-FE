88731ba76a53487125150ab4e9933e762f45955f
leesoyuun <leesoyun4206@naver.com>
2024-02-16 17:55:43 +0900

    Add: 저장목록에서 유저 프로필 들어갈 수있도록 수정

--- src/pages/MyPage/SaveList.js
+++ src/pages/MyPage/SaveList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import API from "../../axios/BaseUrl";
-import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import * as c from "../../components/Common/CommonStyle";
 import Header from "../../components/MyPage/Header";
@@ -20,9 +20,9 @@ const DoneBtn = styled.div`
   height: 4.73vh;
   padding: 0.94vh 3.07vw;
   border-radius: 8px;
-  background: ${(props)=>props.isDone ? '#FFC700' : '#efefef'};
+  background: ${(props) => (props.isDone ? "#FFC700" : "#efefef")};
 
-  color: ${(props)=>props.isDone ? '#333' : '#949494'};
+  color: ${(props) => (props.isDone ? "#333" : "#949494")};
   font-size: 16px;
   font-weight: 600;
   margin-top: 8px;
@@ -54,7 +54,7 @@ const LifeStyles = () => {
   const [saveList, setSaveList] = useState([]);
   const [isDone, setIsDone] = useState(false);
   const [loading, setLoading] = useState(true);
-
+  const navigate = useNavigate();
   const handleEdit = () => {
     setActiveEdit(true);
   };
@@ -62,73 +62,114 @@ const LifeStyles = () => {
   const handleBtn = () => {
     if (checkUserName.length > 0) {
       async function fetchDeleteSaveList() {
-        try{
+        try {
           setLoading(true);
-          const res = await API.get("/roommate/removesave?nickname="+checkUserName);
-          if(res.status == '200'){
+          const res = await API.get(
+            "/roommate/removesave?nickname=" + checkUserName
+          );
+          if (res.status == "200") {
             setLoading(false);
             setShowPopup(true);
-            setSaveList(saveList.filter((data)=> !checkUserName.includes(data.nickname)));
+            setSaveList(
+              saveList.filter((data) => !checkUserName.includes(data.nickname))
+            );
             setCheckUserName([]);
             setIsDone(!isDone);
           }
-          console.log(res)
-        }catch(e) {
+          console.log(res);
+        } catch (e) {
           console.log(e);
-        } 
+        }
       }
       fetchDeleteSaveList();
     }
-    setSaveList(saveList.filter(list => list !== checkUserName));
-  }
-  useEffect(()=>{
+    setSaveList(saveList.filter((list) => list !== checkUserName));
+  };
+  useEffect(() => {
     async function fetchSaveList() {
-      try{
+      try {
         const res = await API.get("/roommate/savelist");
         setSaveList(res.data);
         setLoading(false);
-      }catch(e) {
+      } catch (e) {
         console.log(e);
-      } 
+      }
     }
     fetchSaveList();
-  },[]);
+  }, []);
   const handleCheckIndex = (userName) => {
-    setCheckUserName(value => [...value, userName]);
-    if(checkUserName.includes(userName)) setCheckUserName(checkUserName.filter(nowName => nowName !== userName));
-  }
-  return (
-    loading ? <Loading/> : (
+    setCheckUserName((value) => [...value, userName]);
+    if (checkUserName.includes(userName))
+      setCheckUserName(checkUserName.filter((nowName) => nowName !== userName));
+  };
+  return loading ? (
+    <Loading />
+  ) : (
     <c.Totalframe>
       <c.ScreenComponent>
         <c.SubScreen>
           <c.SpaceBetween>
             {activeEdit ? (
               <>
-                <Header subtitle={`${checkUserName.length}명 선택됨`} andleShow={activeEdit} isCenter={true}/>
-                <DoneBtn isDone={isDone} onClick={()=>setActiveEdit(false)}>완료</DoneBtn>
+                <Header
+                  subtitle={`${checkUserName.length}명 선택됨`}
+                  andleShow={activeEdit}
+                  isCenter={true}
+                />
+                <DoneBtn isDone={isDone} onClick={() => setActiveEdit(false)}>
+                  완료
+                </DoneBtn>
               </>
             ) : (
               <>
-                <Header subtitle={`저장 목록`} andleShow={activeEdit}/>
+                <Header subtitle={`저장 목록`} andleShow={activeEdit} />
                 <EditImg src={Edit} onClick={() => handleEdit()} />
               </>
             )}
           </c.SpaceBetween>
           {/* total save list */}
           <TotalSaveNum>총 {saveList.length}명</TotalSaveNum>
-            {saveList.map((userData)=>(
-              <c.Flex onClick={activeEdit? () => handleCheckIndex(userData.nickname) : null}>
-              {activeEdit && <CheckImg src={checkUserName.includes(userData.nickname) ? Check : NoCheck} />}
-              <OtherProfile activeCheck={checkUserName.includes(userData.nickname)} score={userData.point} userprofile={userData.photoName} nickName={userData.nickname} major={userData.major} id={userData.studentID} intro={userData.introduction} />
-              </c.Flex>
-            ))}
-            {activeEdit && <JoinButton btnName={`삭제하기`} handleClick={() => handleBtn()} isNextPage={checkUserName.length > 0}  />}
-          <Popup bottom={`18.24`} isShowPopup={showPopup} setShowPopup={setShowPopup} message={'성공적으로 삭제되었습니다'}/>
+          {saveList.map((userData) => (
+            <c.Flex
+              onClick={
+                activeEdit ? () => handleCheckIndex(userData.nickname) : null
+              }
+            >
+              {activeEdit && (
+                <CheckImg
+                  src={
+                    checkUserName.includes(userData.nickname) ? Check : NoCheck
+                  }
+                />
+              )}
+              <OtherProfile
+                activeCheck={checkUserName.includes(userData.nickname)}
+                score={userData.point}
+                userprofile={userData.photoName}
+                nickName={userData.nickname}
+                major={userData.major}
+                id={userData.studentID}
+                intro={userData.introduction}
+                onClick={()=>navigate('/detail/details/'+userData.userId)}
+              />
+            </c.Flex>
+          ))}
+          {activeEdit && (
+            <JoinButton
+              btnName={`삭제하기`}
+              handleClick={() => handleBtn()}
+              isNextPage={checkUserName.length > 0}
+            />
+          )}
+          <Popup
+            bottom={`18.24`}
+            isShowPopup={showPopup}
+            setShowPopup={setShowPopup}
+            message={"성공적으로 삭제되었습니다"}
+          />
         </c.SubScreen>
       </c.ScreenComponent>
     </c.Totalframe>
-    )
   );
 };
 export default LifeStyles;
