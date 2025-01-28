e1f422772dd2eb2a0d8540bfff06e07aa202d9be
Hina0222 <hina0222@naver.com>
2025-01-29 02:18:44 +0900

    feat: 달력 컴포넌트 페이지에 따라 구분, 파일 분리

--- src/pages/Calendar/components/CalendarGrid.tsx
+++ src/pages/Calendar/components/CalendarGrid.tsx
@@ -1,66 +1,32 @@
-import { useState } from "react";
-import dayjs, { Dayjs } from "dayjs";
 import styled from "styled-components";
 import { theme } from "../../../styles/theme";
 import Typography from "../../../components/Common/Layouts/Typography";
-import { ReactComponent as RightArrowIcon } from "../.././../assets/img/Calendar/RightArrowIcon.svg";
-import { ReactComponent as LeftArrowIcon } from "../.././../assets/img/Calendar/LeftArrowIcon.svg";
+import { getDaysInMonth, isToday } from "../utils";
+import { useCalendar } from "../hooks/useCalendar";
+import CalendarHeader from "../ui/CalendarHeader";
 
 interface DayProps {
 	$isEmpty: boolean;
 	$isWeekend: boolean;
 	$isSunday: boolean;
+	$isToday: boolean;
 }
 
-export default function CalendarGrid () {
-	const [currentDate, setCurrentDate] = useState<Dayjs>(dayjs());
+interface CalendarGridProps {
+	type: "calendar" | "modal";
+}
 
-	const getDaysInMonth = (): (string | number)[][] => {
-		const year: number = currentDate.year();
-		const month: number = currentDate.month();
-		const firstDay: Dayjs = dayjs(new Date(year, month, 1)); // 월의 첫 번째 날
-		const firstDayOfWeek: number = firstDay.day(); // 첫 번째 날의 요일
-		const daysInMonth: number = currentDate.daysInMonth(); // 월에 포함된 총 날짜 수
-
-		const calendar: (string | number)[][] = [];
-		let day: number = 1;
-
-		for (let i = 0; i < 6; i++) {
-			const week: (string | number)[] = [];
-			for (let j = 0; j < 7; j++) {
-				if (i === 0 && j < firstDayOfWeek) {
-					week.push("");
-				} else if (day > daysInMonth) {
-					week.push("");
-				} else {
-					week.push(day);
-					day++;
-				}
-			}
-			calendar.push(week);
-		}
-		return calendar;
-	};
-
-	const handlePrevMonth = (): void => {
-		setCurrentDate(currentDate.subtract(1, "month"));
-	};
-
-	const handleNextMonth = (): void => {
-		setCurrentDate(currentDate.add(1, "month"));
-	};
+export default function CalendarGrid ({ type }: CalendarGridProps) {
+	const { currentDate, handlePrevMonth, handleNextMonth, handleDayClick } = useCalendar({ type });
 
 	return (
 		<CalendarContainer>
-			<CalendarHeader>
-				<Button onClick={handlePrevMonth}>
-					<LeftArrowIcon />
-				</Button>
-				<Typography typoSize="T3_semibold" color="Gray800">{currentDate.format("YYYY.M")}</Typography>
-				<Button onClick={handleNextMonth}>
-					<RightArrowIcon />
-				</Button>
-			</CalendarHeader>
+			<CalendarHeader
+				type={type}
+				currentDate={currentDate}
+				handlePrevMonth={handlePrevMonth}
+				handleNextMonth={handleNextMonth}
+			/>
 			<WeekdayHeader>
 				{["일", "월", "화", "수", "목", "금", "토"].map((day: string) => (
 					<WeekdayCell key={day} $day={day}>
@@ -69,15 +35,21 @@ export default function CalendarGrid () {
 				))}
 			</WeekdayHeader>
 			<CalendarBody>
-				{getDaysInMonth().map((week: (string | number)[], weekIdx: number) => (
+				{getDaysInMonth(currentDate).map((week: (string | number)[], weekIdx: number) => (
 					<Week key={`week-${weekIdx}`}>
 						{week.map((day: string | number, dayIdx: number) => (
-							<Day key={`day-${weekIdx}-${dayIdx}`}
-								 $isEmpty={day === ""}
-								 $isWeekend={dayIdx === 0 || dayIdx === 6}
-								 $isSunday={dayIdx === 0}
+							<Day
+								key={`day-${weekIdx}-${dayIdx}`}
+								$isEmpty={day === ""}
+								$isWeekend={dayIdx === 0 || dayIdx === 6}
+								$isSunday={dayIdx === 0}
+								$isToday={isToday(currentDate, day)}
+								onClick={() => handleDayClick(day)}
 							>
-								<Typography typoSize="B1_medium">{day}</Typography>
+								<Typography typoSize="B1_medium" color="Gray800">
+									{day}
+								</Typography>
+								<div></div>
 							</Day>
 						))}
 					</Week>
@@ -87,24 +59,7 @@ export default function CalendarGrid () {
 	);
 }
 
-const CalendarContainer = styled.div`
-`;
-
-const CalendarHeader = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-bottom: 36px;
-    gap: 12px;
-`;
-
-const Button = styled.button`
-    border: none;
-    cursor: pointer;
-    padding: 0;
-    display: flex;
-    background-color: transparent;
-`;
+const CalendarContainer = styled.div``;
 
 const WeekdayHeader = styled.div`
     display: flex;
@@ -117,9 +72,9 @@ const WeekdayCell = styled.div<{ $day: string }>`
     text-align: center;
     color: ${({ $day }) => {
         switch ($day) {
-            case "토" :
+            case "토":
                 return theme.Gray500;
-            case "일" :
+            case "일":
                 return theme.Red300;
             default:
                 return theme.Gray600;
@@ -127,24 +82,41 @@ const WeekdayCell = styled.div<{ $day: string }>`
     }};
 `;
 
-const CalendarBody = styled.div`
-`;
+const CalendarBody = styled.div``;
 
 const Week = styled.div`
     display: flex;
     justify-content: space-between;
-	margin-bottom: 12px;
+    margin-bottom: 12px;
 `;
 
 const Day = styled.div<DayProps>`
+    position: relative;
     min-width: 38px;
-	min-height: 48px;
+    min-height: 48px;
     text-align: center;
-	padding: 0 5px 0;
+    padding: 0 5px 0;
     color: ${(props) => {
-        if (props.$isWeekend) {
+        if (props.$isToday) {
+            return theme.Yellow700;
+        } else if (props.$isWeekend) {
             return props.$isSunday ? theme.Red500 : theme.Gray500;
         }
         return theme.Gray800;
     }};
+
+    ${props => !props.$isEmpty && `
+        &:active::after {
+		content: "";
+		position: absolute;
+		top: -6px;
+		left: 50%;
+		transform: translateX(-50%);
+		width: 36px;
+		height: 36px;
+		background-color: ${theme.Gray50};
+        border-radius: 8px;
+        z-index: -1;
+    	}
+	`}
 `;
---
+++ src/pages/Calendar/ui/CalendarHeader.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Typography from "../../../components/Common/Layouts/Typography";
+import styled from "styled-components";
+import { ReactComponent as RightArrowIcon } from "../.././../assets/img/Calendar/RightArrowIcon.svg";
+import { ReactComponent as LeftArrowIcon } from "../.././../assets/img/Calendar/LeftArrowIcon.svg";
+import { ReactComponent as DownArrowIcon } from "../.././../assets/img/Calendar/DownArrowIcon.svg";
+import { Dayjs } from "dayjs";
+import Row from "../../../components/Common/Layouts/Row";
+
+interface CalendarHeaderProps {
+	type: "calendar" | "modal";
+	currentDate: Dayjs;
+	handlePrevMonth: () => void;
+	handleNextMonth: () => void;
+}
+
+export default function CalendarHeader ({ type, currentDate, handlePrevMonth, handleNextMonth }: CalendarHeaderProps) {
+	return (
+		<CalendarHeaderWrapper $type={type}>
+			{type === "calendar" ? (
+				<>
+					<Row verticalAlign="center" gap={4}>
+						<Typography typoSize="H3" color="Gray800">
+							{currentDate.format("YYYY.M")}
+						</Typography>
+						<Button>
+							<DownArrowIcon />
+						</Button>
+					</Row>
+					<Button>
+						<Typography typoSize="B1_semibold" color="Gray800">
+							오늘
+						</Typography>
+					</Button>
+				</>
+			) : (
+				<>
+					<Button onClick={handlePrevMonth}>
+						<LeftArrowIcon />
+					</Button>
+					<Typography typoSize="T3_semibold" color="Gray800">
+						{currentDate.format("YYYY.M")}
+					</Typography>
+					<Button onClick={handleNextMonth}>
+						<RightArrowIcon />
+					</Button>
+				</>
+			)}
+		</CalendarHeaderWrapper>
+	);
+}
+
+const CalendarHeaderWrapper = styled.div<{ $type: "calendar" | "modal" }>`
+    display: flex;
+    align-items: center;
+    justify-content: ${({ $type }) => ($type === "calendar" ? "space-between" : "center")};
+    margin-bottom: ${({ $type }) => ($type === "calendar" ? "16px" : "36px")};
+    gap: ${({ $type }) => ($type === "calendar" ? "0" : "12px")};
+	padding:${({ $type }) => ($type === "calendar" ? "12px 0" : "0")};
+`;
+
+const Button = styled.button`
+    border: none;
+    cursor: pointer;
+    padding: 0;
+    display: flex;
+    background-color: transparent;
+`;
