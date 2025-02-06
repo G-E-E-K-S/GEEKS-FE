d9dee8150b0e338c9e250d8df01731334fa7a1fa
leesoyuun <leesoyun4206@naver.com>
2025-02-06 22:05:02 +0900

    refact(#99): 마이페이지 공지

---
+++ src/pages/MyPage/Notice/Notice.tsx
@@ -0,0 +1,33 @@
+import { useNavigate } from "react-router-dom";
+import * as S from "./style";
+import * as CS from "../../../components/Common/CommonStyle";
+import Header from "../../../components/MyPage/Header";
+import NoticeData from "../../../JSON/notice.json";
+import Column from "../../../components/Common/Layouts/Column";
+import Typography from "../../../components/Common/Layouts/Typography";
+
+export default function Notice() {
+	const navigate = useNavigate();
+	return (
+		<CS.Totalframe>
+			<CS.ScreenComponent>
+				<Header subtitle={`공지사항`} />
+				{NoticeData.notice.map((item) => (
+					<>
+						<div style={{ padding: "20px 0px" }} onClick={() => navigate("/notice/details/event")}>
+							<Column gap={4}>
+								<Typography typoSize="T3_semibold" color="Gray800">
+									{item.noticeName}
+								</Typography>
+								<Typography typoSize="B2_medium" color="Gray400">
+									{item.noticeDate}
+								</Typography>
+							</Column>
+						</div>
+						<S.Br />
+					</>
+				))}
+			</CS.ScreenComponent>
+		</CS.Totalframe>
+	);
+}
