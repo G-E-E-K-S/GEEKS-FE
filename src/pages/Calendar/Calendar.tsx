4299dbdeb88584579ae5616f6e1b747bf5c87ca2
Hina0222 <hina0222@naver.com>
2025-02-15 20:12:43 +0900

    feat: Major 학번 선택 추가

--- src/pages/Calendar/Calendar.tsx
+++ src/pages/Calendar/Calendar.tsx
@@ -16,116 +16,114 @@ import { MONTHS } from "./utils/const";
 import { useNavigate } from "react-router-dom";
 
 const scheduleData = {
-	"2025.1.3": [
-		{ scheduleId: 1, title: "친구와 외출", type: "외출", content: "친구랑 놀러감", time: "13:00 - 17:00" }
-	],
-	"2025.1.15": [
-		{ scheduleId: 2, title: "영화 관람", type: "외출", content: "영화 보러감", time: "14:00 - 18:00" },
-		{ scheduleId: 3, title: "친구집에서 숙박", type: "외박", content: "친구집", time: "18:00 - 다음날 09:00" }
-	],
-	"2025.1.20": [
-		{ scheduleId: 4, title: "카페 방문", type: "외출", content: "카페가기", time: "13:00 - 15:00" },
-		{ scheduleId: 5, title: "가족 모임", type: "외박", content: "가족모임", time: "15:00 - 다음날 10:00" },
-		{ scheduleId: 6, title: "동아리 정기 모임", type: "공동 일정", content: "동아리 모임", time: "19:00 - 21:00" },
-		{ scheduleId: 7, title: "스터디 모임", type: "기타", content: "스터디", time: "10:00 - 12:00" },
-		{ scheduleId: 8, title: "운동 시간", type: "기타", content: "운동", time: "16:00 - 17:00" }
-	],
-	"2025.1.25": [
-		{ scheduleId: 9, title: "친구와 만남", type: "외출", content: "친구 만남", time: "12:00 - 16:00" },
-		{ scheduleId: 10, title: "여행 일정", type: "외박", content: "여행", time: "16:00 - 다음날 18:00" },
-		{ scheduleId: 11, title: "병원 방문", type: "기타", content: "병원가기", time: "10:00 - 11:00" }
-	],
-	"2025.1.30": [
-		{ scheduleId: 12, title: "친구와 만남", type: "외출", content: "친구 만남", time: "12:00 - 16:00" },
-		{ scheduleId: 13, title: "여행 일정", type: "외박", content: "여행", time: "16:00 - 다음날 18:00" },
-		{ scheduleId: 14, title: "병원 방문", type: "기타", content: "병원가기", time: "10:00 - 11:00" }
-	],
-	"2025.2.3": [
-		{ scheduleId: 15, title: "동아리 회식", type: "기타", content: "동아리 회식", time: "18:00 - 20:00" },
-		{ scheduleId: 16, title: "분가하는 날", type: "기타", content: "분가 가는 날", time: "18:00 - 20:00" }
-	],
-	"2025.2.11": [
-		{ scheduleId: 17, title: "동아리 회식", type: "기타", content: "동아리 회식", time: "18:00 - 20:00" },
-		{ scheduleId: 18, title: "분가하는 날", type: "기타", content: "분가 가는 날", time: "18:00 - 20:00" }
-	]
+    "2025.1.3": [
+        { scheduleId: 1, title: "친구와 외출", type: "외출", content: "친구랑 놀러감", time: "13:00 - 17:00" }
+    ],
+    "2025.1.15": [
+        { scheduleId: 2, title: "영화 관람", type: "외출", content: "영화 보러감", time: "14:00 - 18:00" },
+        { scheduleId: 3, title: "친구집에서 숙박", type: "외박", content: "친구집", time: "18:00 - 다음날 09:00" }
+    ],
+    "2025.1.20": [
+        { scheduleId: 4, title: "카페 방문", type: "외출", content: "카페가기", time: "13:00 - 15:00" },
+        { scheduleId: 5, title: "가족 모임", type: "외박", content: "가족모임", time: "15:00 - 다음날 10:00" },
+        { scheduleId: 6, title: "동아리 정기 모임", type: "공동 일정", content: "동아리 모임", time: "19:00 - 21:00" },
+        { scheduleId: 7, title: "스터디 모임", type: "기타", content: "스터디", time: "10:00 - 12:00" },
+        { scheduleId: 8, title: "운동 시간", type: "기타", content: "운동", time: "16:00 - 17:00" }
+    ],
+    "2025.1.25": [
+        { scheduleId: 9, title: "친구와 만남", type: "외출", content: "친구 만남", time: "12:00 - 16:00" },
+        { scheduleId: 10, title: "여행 일정", type: "외박", content: "여행", time: "16:00 - 다음날 18:00" },
+        { scheduleId: 11, title: "병원 방문", type: "기타", content: "병원가기", time: "10:00 - 11:00" }
+    ],
+    "2025.1.30": [
+        { scheduleId: 12, title: "친구와 만남", type: "외출", content: "친구 만남", time: "12:00 - 16:00" },
+        { scheduleId: 13, title: "여행 일정", type: "외박", content: "여행", time: "16:00 - 다음날 18:00" },
+        { scheduleId: 14, title: "병원 방문", type: "기타", content: "병원가기", time: "10:00 - 11:00" }
+    ],
+    "2025.2.3": [
+        { scheduleId: 15, title: "동아리 회식", type: "기타", content: "동아리 회식", time: "18:00 - 20:00" },
+        { scheduleId: 16, title: "분가하는 날", type: "기타", content: "분가 가는 날", time: "18:00 - 20:00" }
+    ],
+    "2025.2.11": [
+        { scheduleId: 17, title: "동아리 회식", type: "기타", content: "동아리 회식", time: "18:00 - 20:00" },
+        { scheduleId: 18, title: "분가하는 날", type: "기타", content: "분가 가는 날", time: "18:00 - 20:00" }
+    ]
 };
 
 export default function Calendar () {
-	let navigate = useNavigate();
+    let navigate = useNavigate();
 
-	const {
-		currentDate,
-		selectedDate,
-		handleDayClick,
-		handleTodayClick,
-		setCurrentDate
-	} = useCalendar();
-	const [isOpen, setIsOpen] = useState<boolean>(false);
+    const {
+        currentDate,
+        selectedDate,
+        handleDayClick,
+        handleTodayClick,
+        setCurrentDate
+    } = useCalendar();
+    const [isOpen, setIsOpen] = useState<boolean>(false);
 
-	const toggleIsOpen = () => {
-		setIsOpen(prevState => !prevState);
-	};
+    const toggleIsOpen = () => {
+        setIsOpen(prevState => !prevState);
+    };
 
-	const handleMonthSelect = (month: number) => {
-		const newDate = currentDate.month(month - 1);
-		setCurrentDate(newDate);
-		setIsOpen(false);
-	};
+    const handleMonthSelect = (month: number) => {
+        const newDate = currentDate.month(month - 1);
+        setCurrentDate(newDate);
+        setIsOpen(false);
+    };
 
-	const todayScheduleDatas = selectedDate
-		? scheduleData[selectedDate]
-		: scheduleData[currentDate.format("YYYY-MM-DD")];
+    const todayScheduleDatas = selectedDate
+        ? scheduleData[selectedDate]
+        : scheduleData[currentDate.format("YYYY-MM-DD")];
 
-export default function Calendar () {
-	return (
-		<c.Totalframe>
-			<c.ScreenComponent navigation={true}>
-				<c.SubScreen>
-					<CalendarHeader
-						type="calendar"
-						currentDate={currentDate}
-						handleTodayClick={handleTodayClick}
-						toggleIsOpen={toggleIsOpen}
-					/>
-					<CalendarGrid
-						type="calendar"
-						scheduleData={scheduleData}
-						currentDate={currentDate}
-						selectedDate={selectedDate}
-						handleDayClick={handleDayClick}
-					/>
-					<Line />
-					<TodaySchedules
-						selectedDate={selectedDate}
-						handleDayClick={handleDayClick}
-						todayScheduleDatas={todayScheduleDatas}
-					/>
-					<BottomSheet isOpen={isOpen} height="45.73vh">
-						<Row horizonAlign="distribute" style={{ marginBottom: "1.25rem" }}>
-							<Typography color="Gray800" typoSize="T2_bold">
-								{"월 선택"}
-							</Typography>
-							<Button onClick={toggleIsOpen}>
-								<CloseModal />
-							</Button>
-						</Row>
-						<ScrollPicker<number, string>
-							options={MONTHS}
-							onOptionSelect={handleMonthSelect}
-							height={220}
-						/>
-					</BottomSheet>
-					<ScheduleAddButton onClick={() => navigate("/schedule/add")}>
-						<AddIcon style={{ width: "20px", height: "20px" }} />
-						<Typography typoSize="T4_semibold" color="Gray700">
-							일정 추가
-						</Typography>
-					</ScheduleAddButton>
-					<CalendarGrid />
-				</c.SubScreen>
-			</c.ScreenComponent>
-		</c.Totalframe>
-	);
+    return (
+        <c.Totalframe>
+            <c.ScreenComponent navigation={true}>
+                <c.SubScreen>
+                    <CalendarHeader
+                        type="calendar"
+                        currentDate={currentDate}
+                        handleTodayClick={handleTodayClick}
+                        toggleIsOpen={toggleIsOpen}
+                    />
+                    <CalendarGrid
+                        type="calendar"
+                        scheduleData={scheduleData}
+                        currentDate={currentDate}
+                        selectedDate={selectedDate}
+                        handleDayClick={handleDayClick}
+                    />
+                    <Line />
+                    <TodaySchedules
+                        selectedDate={selectedDate}
+                        handleDayClick={handleDayClick}
+                        todayScheduleDatas={todayScheduleDatas}
+                    />
+                    <BottomSheet isOpen={isOpen} height="45.73vh">
+                        <Row horizonAlign="distribute" style={{ marginBottom: "1.25rem" }}>
+                            <Typography color="Gray800" typoSize="T2_bold">
+                                {"월 선택"}
+                            </Typography>
+                            <Button onClick={toggleIsOpen}>
+                                <CloseModal />
+                            </Button>
+                        </Row>
+                        <ScrollPicker<number, string>
+                            options={MONTHS}
+                            onOptionSelect={handleMonthSelect}
+                            height={220}
+                        />
+                    </BottomSheet>
+                    <ScheduleAddButton onClick={() => navigate("/schedule/add")}>
+                        <AddIcon style={{ width: "20px", height: "20px" }} />
+                        <Typography typoSize="T4_semibold" color="Gray700">
+                            일정 추가
+                        </Typography>
+                    </ScheduleAddButton>
+                </c.SubScreen>
+            </c.ScreenComponent>
+        </c.Totalframe>
+    );
 }
 
 const Line = styled.div`
--- src/pages/Join/Major/Major.tsx
+++ src/pages/Join/Major/Major.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
 import API from "../../../axios/BaseUrl";
 import styled from "styled-components";
 import * as CS from "../../../components/Common/CommonStyle";
@@ -14,99 +14,131 @@ import Typography from "../../../components/Common/Layouts/Typography";
 import Row from "../../../components/Common/Layouts/Row";
 import DepartmentList from "../../../JSON/DepartmentList.json";
 import BottomSheet from "../../../components/DesignStuff/BottomSheet/BottomSheet";
-import { useUserInfo } from "../../../store/useUserInfo";
+import {useUserInfo} from "../../../store/useUserInfo";
+import ScrollPicker from "../../../components/DesignStuff/ScrollPicker/ScrollPicker";
+import {STUDENT_NUM} from "../const";
 
 const MajorBtsTxt = styled.div`
-	color: #333;
-	font-size: 1.25rem;
-	font-weight: 700;
-	line-height: 28px;
-	margin-bottom: 20px;
+    color: #333;
+    font-size: 1.25rem;
+    font-weight: 700;
+    line-height: 28px;
+    margin-bottom: 20px;
 `;
 const CloseImg = styled.img`
-	width: 28px;
-	height: 28px;
+    width: 28px;
+    height: 28px;
 `;
 const Major = () => {
-	const [isNextPage, setIsNextPage] = useState(false);
-	const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
-	const [isDepartmentOpen, setIsDepartmentOpen] = useState(false);
-	const { major, setMajor, department, setDepartment, studentNum, setStudentNum } = useUserInfo();
-	const navigate = useNavigate();
+    const [isNextPage, setIsNextPage] = useState(false);
+    const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
+    const [isDepartmentOpen, setIsDepartmentOpen] = useState(false);
+    const [isStudentNumOpen, setIsStudentNumOpen] = useState(false);
+    const {major, setMajor, department, setDepartment, studentNum, setStudentNum} = useUserInfo();
+    const navigate = useNavigate();
 
-	const handleBottomSheet = () => {
-		setIsBottomSheetOpen(!isBottomSheetOpen);
-	};
+    const handleBottomSheet = () => {
+        setIsBottomSheetOpen(!isBottomSheetOpen);
+    };
 
-	const openBottomSheet = (department: string) => {
-		setIsDepartmentOpen(!isDepartmentOpen);
-		setDepartment(department);
-	};
-	const handleMajor = (major: string) => {
-		setMajor(major);
-		setIsDepartmentOpen(false);
-		setIsBottomSheetOpen(false);
-	};
+    const openBottomSheet = (department: string) => {
+        setIsDepartmentOpen(!isDepartmentOpen);
+        setDepartment(department);
+    };
 
-	useEffect(() => {
-		setStudentNum(19);
-		major && studentNum ? setIsNextPage(true) : setIsNextPage(false);
-	}, [major, studentNum]);
+    const handleMajor = (major: string) => {
+        setMajor(major);
+        setIsDepartmentOpen(false);
+        setIsBottomSheetOpen(false);
+    };
 
-	return (
-		<CS.Totalframe>
-			<CS.ScreenComponent>
-				<CS.Header backgroundColor={isBottomSheetOpen || isDepartmentOpen ? " rgba(0, 0, 0, 0.5)" : "White"}>
-					<HeaderMenu />
-				</CS.Header>
-				<MainText maintitle={`전공/학과와\n학번을 알려주세요`} />
-				<MajorTotal onClick={() => handleBottomSheet()}>
-					<Typography typoSize="T1" color={major ? "Gray800" : "Gray400"}>
-						{major || "학과/전공"}
-					</Typography>
-					<img src={UnderArrow} />
-				</MajorTotal>
-				<BottomSheet isOpen={isBottomSheetOpen} height={"487px"}>
-					<CS.SpaceBetween>
-						<MajorBtsTxt>{`학과/전공`}</MajorBtsTxt>
-						<CloseImg src={Close} onClick={() => handleBottomSheet()} />
-					</CS.SpaceBetween>
-					{DepartmentList.departmentList.map((department) => (
-						<Department
-							department={department}
-							onClick={() => openBottomSheet(department)}
-							isDepartment={true}
-						/>
-					))}
-				</BottomSheet>
-				{isDepartmentOpen && (
-					<BottomSheet height={`630px`} isOpen={isDepartmentOpen}>
-						<Row horizonAlign="distribute">
-							<Typography typoSize="T2_bold" color="Gray800">
-								{"학과/전공"}
-							</Typography>
-							<CloseImg src={Close} onClick={() => setIsDepartmentOpen(!isDepartmentOpen)} />
-						</Row>
-						{DepartmentList.departmentMajors[department].map((major: string) => (
-							<Department department={major} onClick={() => handleMajor(major)} />
-						))}
-					</BottomSheet>
-				)}
-				<Button text={"다음"} isNextPage={isNextPage} onClick={() => navigate("/gender")} />
-			</CS.ScreenComponent>
-		</CS.Totalframe>
-	);
+    const openStudentNumBottomSheet = () => {
+        setIsStudentNumOpen(!isStudentNumOpen);
+    }
+
+    const handleStudentNum = (studentNum: number) => {
+        setStudentNum(studentNum);
+        setIsStudentNumOpen(false);
+    }
+
+    useEffect(() => {
+        major && studentNum ? setIsNextPage(true) : setIsNextPage(false);
+    }, [major, studentNum]);
+
+    return (
+        <CS.Totalframe>
+            <CS.ScreenComponent>
+                <CS.Header backgroundColor={isBottomSheetOpen || isDepartmentOpen ? " rgba(0, 0, 0, 0.5)" : "White"}>
+                    <HeaderMenu/>
+                </CS.Header>
+                <MainText maintitle={`전공/학과와\n학번을 알려주세요`}/>
+                <MajorTotal onClick={() => handleBottomSheet()}>
+                    <Typography typoSize="T1" color={major ? "Gray800" : "Gray400"}>
+                        {major || "학과/전공"}
+                    </Typography>
+                    <img src={UnderArrow}/>
+                </MajorTotal>
+
+                <StudentNumTotal onClick={() => openStudentNumBottomSheet()}>
+                    <Typography typoSize="T1" color={studentNum ? "Gray800" : "Gray400"}>
+                        {studentNum || "학번"}
+                    </Typography>
+                    <img src={UnderArrow}/>
+                </StudentNumTotal>
+                <BottomSheet isOpen={isStudentNumOpen} height={"487px"}>
+                    <CS.SpaceBetween>
+                        <MajorBtsTxt>{`학번`}</MajorBtsTxt>
+                        <CloseImg src={Close} onClick={() => openStudentNumBottomSheet()}/>
+                    </CS.SpaceBetween>
+                    <ScrollPicker options={STUDENT_NUM} height={220} onOptionSelect={handleStudentNum}/>
+                </BottomSheet>
+
+                <BottomSheet isOpen={isBottomSheetOpen} height={"487px"}>
+                    <CS.SpaceBetween>
+                        <MajorBtsTxt>{`학과/전공`}</MajorBtsTxt>
+                        <CloseImg src={Close} onClick={() => handleBottomSheet()}/>
+                    </CS.SpaceBetween>
+                    {DepartmentList.departmentList.map((department) => (
+                        <Department
+                            department={department}
+                            onClick={() => openBottomSheet(department)}
+                            isDepartment={true}
+                        />
+                    ))}
+                </BottomSheet>
+                {isDepartmentOpen && (
+                    <BottomSheet height={`630px`} isOpen={isDepartmentOpen}>
+                        <Row horizonAlign="distribute">
+                            <Typography typoSize="T2_bold" color="Gray800">
+                                {"학과/전공"}
+                            </Typography>
+                            <CloseImg src={Close} onClick={() => setIsDepartmentOpen(!isDepartmentOpen)}/>
+                        </Row>
+                        {DepartmentList.departmentMajors[department].map((major: string) => (
+                            <Department department={major} onClick={() => handleMajor(major)}/>
+                        ))}
+                    </BottomSheet>
+                )}
+                <Button text={"다음"} isNextPage={isNextPage} onClick={() => navigate("/gender")}/>
+            </CS.ScreenComponent>
+        </CS.Totalframe>
+    );
 };
 
 export default Major;
 
 const MajorTotal = styled.div`
-	margin-top: 6.16vh;
-	padding: 7px 0px 8px 0px;
-	display: flex;
-	justify-content: space-between;
-	width: 100%;
-	border-bottom: 2px solid #efefef;
+    margin-top: 6.16vh;
+    padding: 7px 0px 8px 0px;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    border-bottom: 2px solid #efefef;
+`;
+
+const StudentNumTotal = styled(MajorTotal)`
+    width: fit-content;
+    gap: 30px;
 `;
 // const MajorText = styled.div`
 // 	color: ${(props) => (props.major ? "#d0d0d0" : "#333333")};
